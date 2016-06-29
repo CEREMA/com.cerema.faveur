f68a948172a1de925efc880faa410c745d75560e
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-06-29 19:01:35 +0200

    Update# 7

--- src/Contents/Application/app/view/VMain.js
+++ src/Contents/Application/app/view/VMain.js
@@ -23,38 +23,9 @@ App.view.define('VMain', {
 			split:true,
 			layout: "fit",
 			items: [
-{
-				xtype: "schedulergrid",
-				flex: 1,
-				width: "100%",
-			    startDate     : new Date(2016, 5, 1),
-				endDate       : new Date(2016, 5, 7),
-				startTime     : 6,
-				endTime       : 19,
-				resourceStore : App.resourcestore.create({
-					data:[        
-							{ Id : 'r0', Name : 'Unassigned', Color : '#000' }
-						 ]
-				}),
-				eventStore    : App.eventstore.create({
-					fields:[],
-					data:[]
-				}),
-				style         : 'border: 1px solid #d0d0d0;',
-
-				showTodayLine : true,
-				viewPreset   : 'hourAndDay',
-
-    			calendarColumnClass : 'App.column.Day',
-				//calendarViewPreset   : 'day',
-				mode                 : 'calendar',
-				eventResizeHandles   : 'end',
-				eventBodyTemplate    : '{Name}',
-				snapToIncrement      : true,
-				highlightCurrentTime : true,
-				calendarTimeAxisCfg  : {
-        			height : 30
-    			}
+			{
+				html: "hello world !",
+				padding: 10
 			}
 			]
 		}
