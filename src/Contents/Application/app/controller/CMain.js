466e9ea77ee78f70bbdaa2d943aeb88565557a06
Stéphane Zucatti <stephane.zucatti@me.com>
2016-07-02 23:15:28 +0200

    Update# 41

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -38,8 +38,8 @@ App.controller.define('CMain', {
 	{
 		var cme=App.get('mainform textfield#cme').getValue()*1;
 		var epaisseur=App.get('mainform textfield#epaisseur').getValue()*1;
-		if (cme<5) App.get('mainform textfield#cint')
-	},
+		if (cme<5) App.get('mainform textfield#cint').setValue(cme*epaisseur/10); else App.get('mainform textfield#cint').setValue(5*epaisseur/10);
+ 	},
 	onLoad: function()
 	{
 		// form loaded	
