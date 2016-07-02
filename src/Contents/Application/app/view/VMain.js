9eb522bb1e47edeeec3d2da8fee679e90dc721a5
Stéphane Zucatti <stephane.zucatti@me.com>
2016-07-02 22:46:38 +0200

    Update# 23

--- src/Contents/Application/app/view/VMain.js
+++ src/Contents/Application/app/view/VMain.js
@@ -95,7 +95,62 @@ App.view.define('VMain', {
 			{
 				title: "Montpellier",
 				xtype: "grid",
-				columns: [],
+				columns: [
+				{
+					header: "CR annuel"
+				},
+				{
+					header: "CR été"
+				},
+				{
+					header: "CR hiver"
+				},
+				{
+					header: "CR ev >1mm"
+				},
+				{
+					header: "CR ev >1mm<br>été"
+				},
+				{
+					header: "CR ev >1mm<br>hiver"
+				},
+				{
+					header: "CR ev >5mm"
+				},
+				{
+					header: "CR ev > 5mm<br>été"
+				},	
+				{
+					header: "CR ev > 5mm<br>hiver"
+				},	
+				{
+					header: "Ab annuel"
+				},	
+				{
+					header: "AB<br>été"
+				},	
+				{
+					header: "AB<br>hiver"
+				},	
+				{
+					header: "AB ev >1mm"
+				},	
+				{
+					header: "AB ev >1mm<br>été"
+				},	
+				{
+					header: "AB ev >1mm<br>hiver"
+				},
+				{
+					header: "AB ev >5mm"
+				},	
+				{
+					header: "AB ev >5mm<br>été"
+				},	
+				{
+					header: "AB ev >5mm<br>hiver"
+				}	
+				],
 				width: "100%",
 				store: App.store.create({fields:[],data:[]}),
 				flex: 1
