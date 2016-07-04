c5f16005fed40bbeccd1d20d17b0f2853317c7b8
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-07-04 11:00:23 +0200

    Update# 70

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -39,6 +39,12 @@ App.controller.define('CMain', {
 		var cme=App.get('mainform textfield#cme').getValue()*1;
 		var epaisseur=App.get('mainform textfield#epaisseur').getValue()*1;
 		if (epaisseur<5) App.get('mainform textfield#cint').setValue(cme*epaisseur/10); else App.get('mainform textfield#cint').setValue(5*cme/10);
+        var obj={
+            
+        };
+        App.Calcul.getAll('',function(result){
+            console.log(result); 
+        });
  	},
 	onLoad: function()
 	{
